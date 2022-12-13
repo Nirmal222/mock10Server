915cb3adb6650a1388f7b1c7aa4342bf5adf5639
G Nirmal Kumar <33796030+Nirmal222@users.noreply.github.com>
2022-12-13 14:36:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+const {userRoutes, emiRoutes} = require("./Routes/index");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/user",userRoutes);
+app.use("/calculateEMI",emiRoutes)
+mongoose.connect("mongodb+srv://Nirmal:nirmal222@cluster0.pk8wxgz.mongodb.net/mock10").then(()=>{
+    app.listen(8080, () => {console.log('server started at http://localhost:8080')});
+})
