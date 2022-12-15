e6342957c55e4d44a650c0b3915673d03f923146
G Nirmal Kumar <33796030+Nirmal222@users.noreply.github.com>
2022-12-15 12:24:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require('mongoose');
-const {userRoutes, emiRoutes} = require("./Routes/index");
+const productRoutes = require("./routes/productRoute");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/user",userRoutes);
-app.use("/calculateEMI",emiRoutes)
-app.use("/",(req,res)=>{
-    res.send("Welcome")
-})
-mongoose.connect("mongodb+srv://Nirmal:nirmal222@cluster0.pk8wxgz.mongodb.net/mock10").then(()=>{
+app.use("/products",productRoutes);
+
+mongoose.connect("mongodb+srv://Nirmal:nirmal222@cluster0.pk8wxgz.mongodb.net/mock11").then(()=>{
     app.listen(8080, () => {console.log('server started at http://localhost:8080')});
 })
